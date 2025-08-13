f41e8fe3ec10a7b645ecec34737635490d3e4871
Sanika Tiwari <96829586+humaninexistance@users.noreply.github.com>
2025-08-13 15:24:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-function charge(){
-  var charge = Number(document.getElementById("proton").value)-Number(document.getElementById("electron").value);
-  console.log("the net charge of the atom/ion is " + charge);
+function charge() {
+  const protons = Number(document.getElementById("protons").value);
+  const electrons = Number(document.getElementById("electrons").value);
+  const netCharge = protons - electrons;
+  console.log("the net charge of the atom/ion is " + netCharge);
+  document.getElementById("charge").innerText = netCharge;
 }
 
-function atom(){
-  var charge = Number(document.getElementById("proton").value)-Number(document.getElementById("electron").value);
-  if ((local)var charge = 0){
-    var atomorion = atom;
+function atom() {
+  const protons = Number(document.getElementById("protons").value);
+  const electrons = Number(document.getElementById("electrons").value);
+  const netCharge = protons - electrons;
+  
+  let atomorion;
+  if (netCharge === 0) {
+    atomorion = "Atom";
+  } else if (netCharge > 0) {
+    atomorion = "Positive ion";
+  } else if (netCharge < 0) {
+    atomorion = "Negative ion";
   } else {
-    if ((local)var charge > 0){
-      var atomorion = positive ion;
-    } else {
-      if((local)var charge < 0){
-        var atomorion = negative ion;
-      } else {
-        var atomorion = "i don't know, ask sanika";
-      }
-    }
+    atomorion = "I don't know, ask Sanika";
   }
+
   console.log("this is an " + atomorion);
+  document.getElementById("atomorion").innerText = atomorion;
 }
 
-function mass(){
-  var mass = Number(document.getElementById("proton").value)+Number(document.getElementById("neutron").value);
-  console.log("the mass of this atom or ion is " + mass);
+function mass() {
+  const protons = Number(document.getElementById("protons").value);
+  const neutrons = Number(document.getElementById("neutrons").value);
+  const atomicMass = protons + neutrons;
+  console.log("the mass of this atom or ion is " + atomicMass);
+  document.getElementById("mass").innerText = atomicMass;
 }
